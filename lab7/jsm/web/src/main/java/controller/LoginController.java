744406eade70c7c5e8c6ebc264ada38d6797cafb
DanielVeB <danielkurosz@gmail.com>
2020-05-12 22:11:31 +0200

    Update web module. Update jsf files and controller

--- lab7/jsm/web/src/main/java/controller/LoginController.java
+++ lab7/jsm/web/src/main/java/controller/LoginController.java
@@ -1,23 +1,25 @@
 package controller;
 
+import entity.User;
 import exception.InvalidPasswordException;
 import exception.UserNotFoundException;
 import service.ILoginService;
+import util.SessionUtils;
 
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 import java.io.Serializable;
 
-@ManagedBean(name = "login")
+@ManagedBean(name = "loginController")
 @SessionScoped
 public class LoginController implements Serializable {
 
     @EJB
     private ILoginService loginService;
-
 
     private String user;
     private String pwd;
@@ -38,20 +40,17 @@ public class LoginController implements Serializable {
         this.pwd = pwd;
     }
 
-
     public String loginUser() {
         try {
-            loginService.loginUser(user,pwd);
-
-        }catch (InvalidPasswordException ex){
+            User user = loginService.loginUser(this.user, this.pwd);
+            HttpSession session = SessionUtils.getSession();
+            session.setAttribute("username", user);
+            return "app/forum";
+        } catch (UserNotFoundException | InvalidPasswordException e) {
             FacesContext.getCurrentInstance().addMessage("loginForm:login",
-                    new FacesMessage("Invalid password, Please try again"));
-        }catch (UserNotFoundException ex){
-            FacesContext.getCurrentInstance().addMessage("loginForm:login",
-                    new FacesMessage("User with this name not found"));
+                    new FacesMessage("Invalid user or password, Please try again"));
+            return "login";
         }
-        return "";
     }
-
 
 }
