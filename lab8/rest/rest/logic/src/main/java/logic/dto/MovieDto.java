a8a72ffb5e9a91b00a3a55759a03a8176844ee06
DanielVeB <danielkurosz@gmail.com>
2020-05-23 22:22:01 +0200

    [LAB_8] Add dto classes

---
+++ lab8/rest/rest/logic/src/main/java/logic/dto/MovieDto.java
@@ -0,0 +1,37 @@
+package logic.dto;
+
+import java.util.UUID;
+
+public class MovieDto {
+
+    private UUID id;
+    private String title;
+    private String url;
+
+    public MovieDto() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ lab8/rest/rest/logic/src/main/java/logic/dto/UserDto.java
@@ -0,0 +1,36 @@
+package logic.dto;
+
+
+import java.util.UUID;
+
+public class UserDto {
+
+    private UUID id;
+    private String name;
+    private int age;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
